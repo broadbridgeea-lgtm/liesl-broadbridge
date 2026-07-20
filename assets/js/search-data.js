bd53d561bcdd075b15cb100046ec3c7736232788
broadbridgeea-lgtm <broadbridgeea@gmail.com>
2026-07-20 20:58:38 +0000

    Deploying to gh-pages from @ broadbridgeea-lgtm/liesl-broadbridge@d9d8219f593884323ca5ee6727bf4c947578b3b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This page summarizes highlights.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/liesl-broadbridge/cv/";
